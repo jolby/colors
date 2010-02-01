ef9a528b4f4bc0c9accd54d01397d251ccc478f1
Joel Boehland <joel.boehland@evocomputing.com>
2010-02-01 14:31:19 -0800

    project.clj file for colors

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.evocomputing/colors
+  "1.0.0-SNAPSHOT"
+  :description "Utilities for color manipulations. Inspired by the color module in SASS."
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
+  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
