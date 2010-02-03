5117e898fe55cfc07bcc44348849f8dfbb9692c3
Joel Boehland <joel.boehland@evocomputing.com>
2010-02-03 12:49:16 -0800

    org->com namespace

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject org.evocomputing/colors
   "1.0.0-SNAPSHOT"
-  :description "Utilities for color manipulations. Inspired by the color module in SASS."
+  :description "Utilities for color manipulations.
+This is mostly code ported from  the color module in SASS."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
