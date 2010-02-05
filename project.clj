3d3f1673fd0b9a59ac70a565b9fa10f283a7c850
Joel Boehland <joel.boehland@evocomputing.com>
2010-02-04 17:26:00 -0800

    Add autodoc deps and config

--- project.clj
+++ project.clj
@@ -4,4 +4,12 @@
 This is mostly code ported from  the color module in SASS."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
-  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]])
+  :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]
+                     [autodoc "0.7.0"]]
+
+  :autodoc { :name "colors"
+            :description "Color and colorspace calculation, manipulation and conversion in Clojure."
+            :page-title "Colors API documentation"
+            :copyright "Eclipse Public License (EPL)"
+            :web-src-dir "http://github.com/jolby/colors/blob/"
+            :web-home "http://jolby.github.com/colors/"})
