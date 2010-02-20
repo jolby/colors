7cfea962263f9150a7eb3d41d9cc2c4771e1e415
Joel Boehland <joel.boehland@evocomputing.com>
2010-02-20 11:51:31 -0800

    Added implementations of mostly cargo-cult copied functions over from R's color_pallete package. hsl-rainbow, hsl-sequence, hsl-heat, hsl-terrain.

--- src/com/evocomputing/colors.clj
+++ src/com/evocomputing/colors.clj
@@ -511,18 +511,108 @@ color - a new color that is the result of the binary operation."
 (defn opposite [color]
   (adjust-hue color 180))
 
+
+(defn inclusive-seq [n start end]
+  "Return n evenly spaced points along the range start - end (inclusive)"
+  (loop [acc [] step (/ (- end start) (- n 1)) num start idx 0]
+    (if (= idx n) acc
+        (recur (conj acc num) step (+ step num) (inc idx)))))
+
 (defn rainbow-hsl
-  ""
+  "Computes a rainbow of colors (qualitative palette) defined by
+different hues given a single value of each saturation and lightness.
+
+Arguments:
+numcolors: Number of colors to be produced in this pallette.
+
+Optional Arguments:
+:s (keyword) saturation. 0.0 - 100.0 (default 50.0)
+:l (keyword) lightness. 0.0 - 100.0. (default 70.0)
+:start (keyword) hue where rainbow starts. 0 - 360 (default 0)
+:end (keyword) hue where rainbow ends. 0 - 360 (default: (* 360 (/ (- numcolors 1) numcolors)))
+
+"
   [numcolors & opts]
-  (let [opts (merge {:s 50.0 :l 70.0 :start 0 :end (* 360 (/ (- numcolors 1) numcolors))} opts)
-        hvals (range (:start opts) (inc (:end opts)) (/ (:end opts) (dec numcolors)))]
+  (let [opts (merge {:s 50.0 :l 70.0 :start 0 :end (* 360 (/ (- numcolors 1) numcolors))}
+                    (when opts (apply assoc {} opts)))
+        ;;hvals (range (:start opts) (inc (:end opts)) (/ (:end opts) (dec numcolors)))
+        hvals (inclusive-seq numcolors (:start opts) (:end opts))]
     (printf (format "hvals: %s" (vec hvals)))
     (map #(create-color :h (float %) :s (:s opts) :l (:l opts))
          hvals)))
 
 (defn sequential-hsl
-  ""
-  [numcolors & opts])
+  "Creates a sequential palette starting at the full color
+   (h :s-start :l-start) through to a light color (h :s-end :l-end) by
+   interpolation.
+
+Arguments:
+numcolors: Number of colors to be produced in this pallette.
+
+Optional Arguments:
+:h (keyword) starting hue (default 260)
+:s-start (keyword) starting saturation. 0.0 - 100.0 (default 80.0)
+:l-start (keyword) starting lightness. 0.0 - 100.0. (default 30.0)
+:s-end (keyword) ending saturation. 0.0 - 100.0 (default 0.0)
+:l-end (keyword) ending lightness. 0.0 - 100.0. (default 90.0)
+:power (keyword) control parameter determining how chroma and luminance should
+be increased (1 = linear, 2 = quadratic, etc.)
+"
+
+  [numcolors & opts]
+  (let [opts (merge {:h 260
+                     :s-start 80.0 :l-start 30.0
+                     :s-end 0.0 :l-end 90.0 :power 1.5}
+                    (when opts (apply assoc {} opts)))
+        diff-s (- (:s-end opts) (:s-start opts))
+        diff-l (- (:l-end opts) (:l-start opts))]
+    (map #(create-color :h (:h opts)
+                        :s (- (:s-end opts) (* diff-s (Math/pow % (:power opts))))
+                        :l (- (:l-end opts) (* diff-l (Math/pow % (:power opts)))))
+         (inclusive-seq numcolors 1.0 0.0))))
+
+(defn heat-hsl
+  " Create heat pallette in HSL space. By default, it goes from a red to a yellow hue, while
+     simultaneously going to lighter colors (i.e., increasing
+     lightness) and reducing the amount of color (i.e., decreasing
+     saturation).
+
+Arguments:
+numcolors: Number of colors to be produced in this pallette.
+
+Optional Arguments:
+:h-start (keyword) starting hue (default 260)
+:h-end (keyword) ending hue (default 260)
+:s-start (keyword) starting saturation. 0.0 - 100.0 (default 80.0)
+:l-start (keyword) starting lightness. 0.0 - 100.0. (default 30.0)
+:s-end (keyword) ending saturation. 0.0 - 100.0 (default 0.0)
+:l-end (keyword) ending lightness. 0.0 - 100.0. (default 90.0)
+:power-saturation (keyword) control parameter determining how saturation should increase
+:power-lightness (keyword) control parameter determining how lightness should increase
+be increased (1 = linear, 2 = quadratic, etc.)
+"
+
+  [numcolors & opts]
+  (let [opts (merge {:h-start 0 :h-end 90
+                     :s-start 80.0 :l-start 30.0
+                     :s-end 0.0 :l-end 90.0
+                     :power-saturation 0.20 :power-lightness 1}
+                    (when opts (apply assoc {} opts)))
+        diff-h (- (:h-end opts) (:h-start opts))
+        diff-s (- (:s-end opts) (:s-start opts))
+        diff-l (- (:l-end opts) (:l-start opts))]
+    (map #(create-color :h (- (:h-end opts) (* (- diff-h %)))
+                        :s (- (:s-end opts) (* diff-s (Math/pow % (:power-saturation opts))))
+                        :l (- (:l-end opts) (* diff-l (Math/pow % (:power-lightness opts)))))
+         (inclusive-seq numcolors 1.0 0.0))))
+
+(defn terrain-hsl
+  "The 'terrain_hcl' palette simply calls 'heat_hcl' with
+different parameters, providing colors "
+  [numcolors & opts]
+  (heat-hsl numcolors :h-start 0 :h-end 90
+            :s-start 100.0 :s-end 30.0
+            :l-start 50.0 :l-end 90.0))
 
 (def html4-colors-name-to-rgbnum
      {
