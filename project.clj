1d5f8e6e36e947485bb760d5c145fbc9ff9c024b
Joel Boehland <joel.boehland@evocomputing.com>
2010-04-16 16:34:58 -0700

    Moved project resources not needed in jar file to priv/resources

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@ This is mostly code ported from  the color module in SASS."
   :dependencies [[org.clojure/clojure "1.1.0"]
                  [org.clojure/clojure-contrib "1.0-SNAPSHOT"]]
   :dev-dependencies [[leiningen/lein-swank "1.0.0-SNAPSHOT"]
-                     [autodoc "0.7.0"]]
+                     [autodoc "0.7.0"]
+                     [lein-clojars "0.5.0-SNAPSHOT"]]
 
   :autodoc { :name "colors"
             :description "Color and colorspace calculation, manipulation and conversion in Clojure."
