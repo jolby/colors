18d98d8d99a78458bb9edf7c2ad4287175fd0b16
James Elliott <james.elliott@singlewire.com>
2015-04-21 21:17:27 -0500

    Update to current libraries.
    Actually, a beta version of Clojure 1.7, will wait until that is
    released before making a pull request. But I want this on Clojars, at
    least as a non-Canonical fork, so I can continue working on Afterglow in
    the interim.

--- project.clj
+++ project.clj
@@ -1,14 +1,16 @@
-(defproject com.evocomputing/colors
-  "1.0.1-SNAPSHOT"
+(defproject org.clojars.brunchboy/colors
+  "1.0.2-SNAPSHOT"
   :description "Utilities for color manipulations.
 This is mostly code ported from  the color module in SASS."
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/math.numeric-tower "0.0.1"]
-                 [org.clojure/core.incubator "0.1.2"]]
+  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]
+                 [org.clojure/core.incubator "0.1.3"]]
 
   :autodoc { :name "colors"
             :description "Color and colorspace calculation, manipulation and conversion in Clojure."
             :page-title "Colors API documentation"
             :copyright "Eclipse Public License (EPL)"
             :web-src-dir "http://github.com/jolby/colors/blob/"
-            :web-home "http://jolby.github.com/colors/"})
+            :web-home "http://jolby.github.com/colors/"}
+  :plugins [[lein-ancient "0.6.5"]]
+  :min-lein-version "2.0.0")
