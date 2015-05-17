09005f1e80622a35b31e30ad7872192e56a9e218
James Elliott <james.elliott@singlewire.com>
2015-05-16 21:09:05 -0500

    Add codex API docs, update string package location

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
 This is mostly code ported from  the color module in SASS."
   :url "http://jolby.github.com/colors/"
   :license "Eclipse Public License (EPL)"
-  :dependencies [[org.clojure/clojure "1.7.0-beta1"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/core.incubator "0.1.3"]]
 
@@ -14,5 +14,9 @@ This is mostly code ported from  the color module in SASS."
             :copyright "Eclipse Public License (EPL)"
             :web-src-dir "http://github.com/jolby/colors/blob/"
             :web-home "http://jolby.github.com/colors/"}
-  :plugins [[lein-ancient "0.6.5"]]
+  :plugins [[lein-ancient "0.6.5"]
+            [codox "0.8.12"]]
+  :codox {:src-dir-uri "http://github.com/brunchboy/colors/blob/master/"
+          :src-linenum-anchor-prefix "L"
+          :output-dir "target/doc"}
   :min-lein-version "2.0.0")
