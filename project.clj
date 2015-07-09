60dd9a335e00f2f79dc4fcf07644b9ce50abd7c1
James Elliott <james.elliott@singlewire.com>
2015-07-09 09:56:49 -0500

    Prepare for pull request.

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject org.clojars.brunchboy/colors
-  "1.0.2-SNAPSHOT"
+(defproject com.evocomputing/colors
+  "1.0.2"
   :description "Utilities for color manipulations.
 This is mostly code ported from  the color module in SASS."
   :url "http://jolby.github.com/colors/"
   :license "Eclipse Public License (EPL)"
-  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
+  :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/math.numeric-tower "0.0.4"]
                  [org.clojure/core.incubator "0.1.3"]]
 
@@ -14,9 +14,9 @@ This is mostly code ported from  the color module in SASS."
             :copyright "Eclipse Public License (EPL)"
             :web-src-dir "http://github.com/jolby/colors/blob/"
             :web-home "http://jolby.github.com/colors/"}
-  :plugins [[lein-ancient "0.6.5"]
+  :plugins [[lein-ancient "0.6.7"]
             [codox "0.8.12"]]
-  :codox {:src-dir-uri "http://github.com/brunchboy/colors/blob/master/"
+  :codox {:src-dir-uri "https://github.com/jolby/colors/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-dir "target/doc"}
   :min-lein-version "2.0.0")
