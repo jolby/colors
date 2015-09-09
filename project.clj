5d49621001755b1ad444d1db9bbecb0f769dcb8d
Joel Boehland <jboehland@gmail.com>
2015-09-09 14:41:24 -0700

    bump version

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject com.evocomputing/colors
-  "1.0.2"
+  "1.0.3"
   :description "Utilities for color manipulations.
 This is mostly code ported from  the color module in SASS."
   :url "http://jolby.github.com/colors/"
