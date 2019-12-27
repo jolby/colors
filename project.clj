47e90975430d23a47dbc741f238902acdfec18ce
James Elliott <james@deepsymmetry.org>
2019-12-27 16:53:33 -0600

    Fix compiler warnings with current versions of Clojure
    The `seqable?` function is now part of clojure.core, so including this
    older version from clojure.contrib is causing compile-time warnings.
    This also eliminates the need to depend on clojure.contrib at all.
    
    I also removed `lein-ancient` from the project plugins. The version
    referenced crashes with current Java and Clojure distributions, and
    this tool is something that better fits in an individual's
    `~/.lein/profiles.clj` rather than in the project definition.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject com.evocomputing/colors
-  "1.0.4"
+  "1.0.5"
   :description "Utilities for color manipulations.
 This is mostly code ported from  the color module in SASS."
   :url "http://jolby.github.com/colors/"
   :license "Eclipse Public License (EPL)"
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/math.numeric-tower "0.0.4"]
-                 [org.clojure/core.incubator "0.1.3"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/math.numeric-tower "0.0.4"]]
 
   :autodoc { :name "colors"
             :description "Color and colorspace calculation, manipulation and conversion in Clojure."
@@ -14,8 +13,7 @@ This is mostly code ported from  the color module in SASS."
             :copyright "Eclipse Public License (EPL)"
             :web-src-dir "http://github.com/jolby/colors/blob/"
             :web-home "http://jolby.github.com/colors/"}
-  :plugins [[lein-ancient "0.6.7"]
-            [codox "0.8.12"]]
+  :plugins [[codox "0.8.12"]]
   :codox {:src-dir-uri "https://github.com/jolby/colors/blob/master/"
           :src-linenum-anchor-prefix "L"
           :output-dir "target/doc"}
